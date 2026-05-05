d70bb563a85d7fdfbb71dddb08fcd0ecff34eb51
Francisco Mendez <136205904+Frankmendez10@users.noreply.github.com>
2026-05-05 12:01:50 -0600

    Create SeguimientoRequest.cs

---
+++ 02-CustomAPIs/Models/SeguimientoRequest.cs
@@ -0,0 +1,9 @@
+// Request.cs
+namespace CustomAPIs.ActualizarSeguimiento.Models
+{
+    public class Request
+    {
+        public string IdSeguimiento { get; set; }
+        public string NumeroCaso { get; set; }
+    }
+}
