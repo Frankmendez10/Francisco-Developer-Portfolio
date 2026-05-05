a911aaa85a1e5963b97d719e0215b1fdc4f4d4ae
Francisco Mendez <136205904+Frankmendez10@users.noreply.github.com>
2026-05-05 12:03:51 -0600

    Delete 02-CustomAPIs/Models/SeguimientoResponse.cs

--- 02-CustomAPIs/Models/SeguimientoResponse.cs
+++
@@ -1,9 +0,0 @@
-// Response.cs
-namespace CustomAPIs.ActualizarSeguimiento.Models
-{
-    public class Response
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-    }
-}
