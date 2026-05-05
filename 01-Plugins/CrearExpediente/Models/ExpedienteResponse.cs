9cc18322b7d56ea353346d6e2c82b68fb74d3c63
Francisco Mendez <136205904+Frankmendez10@users.noreply.github.com>
2026-05-05 11:40:31 -0600

    Create ExpedienteResponse.cs

---
+++ 01-Plugins/CrearExpediente/Models/ExpedienteResponse.cs
@@ -0,0 +1,9 @@
+namespace Plugins.CrearExpediente.Models
+{
+    public class ExpedienteResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string NumeroExpediente { get; set; }
+    }
+}
